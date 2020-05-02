392ad1f9586181d0c55f7fef20573cc8cfaf39e9
slvKhan <hanviacheslaw@gmail.com>
2020-05-02 15:58:42 +0300

    ref(test)

--- __test__/index.test.js
+++ __test__/index.test.js
@@ -7,22 +7,27 @@ const callback2 = (el) => el + 1;
 const callback3 = (el) => el > 4;
 
 test('myMap', () => {
+	expect(map([], callback)).toHaveLength(0);
 	expect(map(list, callback)).toEqual(list.map(callback));
 	expect(map(list, callback2)).toEqual(list.map(callback2));
 });
 
-test('asyncMap', () => {
+test('async', () => {
+	mapAsync([], callback, (res) => {
+		expect(res).toHaveLength(0);
+	});
 	mapAsync(list, callback, (res) => {
 		expect(res).toEqual(list.map(callback));
 	});
-})
-
-test('asyncFilter', () => {
+	filterAsync([], callback3, (res) => {
+		expect(res).toHaveLength(0);
+	});
 	filterAsync(list, callback3, (res) => {
 		expect(res).toEqual(list.filter(callback3));
 	});
-})
+});
 
 test('myFilter', () => {
+	expect(filter([], callback3)).toHaveLength(0);
 	expect(filter(list, callback3)).toEqual(list.filter(callback3));
 });
