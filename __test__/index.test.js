6135fa9552fe581d73485d3127f0dcd70e950c4d
slvKhan <hanviacheslaw@gmail.com>
2020-05-02 15:46:16 +0300

    ref(test)

--- __test__/index.test.js
+++ __test__/index.test.js
@@ -2,32 +2,27 @@ import { map, mapAsync } from '../src/map';
 import { filter, filterAsync } from '../src/filter';
 
 const list = [1, 5, 7, -5, 0];
+const callback = (el) => el * 2;
+const callback2 = (el) => el + 1;
+const callback3 = (el) => el > 4;
 
 test('myMap', () => {
-	const callback = (el) => el * 2;
-	const callback2 = (el) => el + 1;
-
 	expect(map(list, callback)).toEqual(list.map(callback));
 	expect(map(list, callback2)).toEqual(list.map(callback2));
 });
 
 test('asyncMap', () => {
-	const callback = (el) => el * 2;
-
 	mapAsync(list, callback, (res) => {
 		expect(res).toEqual(list.map(callback));
 	});
 })
 
 test('asyncFilter', () => {
-	const callback = (el) => el > 4;
-
-	filterAsync(list, callback, (res) => {
-		expect(res).toEqual(list.filter(callback));
+	filterAsync(list, callback3, (res) => {
+		expect(res).toEqual(list.filter(callback3));
 	});
 })
 
 test('myFilter', () => {
-	const callback = (el) => el > 4;
-	expect(filter(list, callback)).toEqual(list.filter(callback));
+	expect(filter(list, callback3)).toEqual(list.filter(callback3));
 });
